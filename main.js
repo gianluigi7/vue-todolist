5f084933b65d95a3e4ff05b32130bbb48fab0239
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-22 21:53:52 +0200

    bonus (ho dimenticato di committare)

--- main.js
+++ main.js
@@ -6,7 +6,34 @@ createApp({
     data() {
         return {
             text: 'Todolist',
+            inputTodo: '',
+            statuses: ['not done', 'done'],
+            todoList: [],
+            //isDone: false,
         }
+    },
+    methods: {
+        addTodo() {
+         if (this.inputTodo) {
+             this.todoList.push({
+                name: this.inputTodo,
+                status: 'not done'
+             });
+             this.inputTodo = '';
+            }
+        },
+        deleteTodo(index) {
+            
+          this.todoList.splice(index, 1);
+        },
+        doneTodo(index) {
+           let newIndex = this.statuses.indexOf(this.todoList[index].status)
+           if(++newIndex > 1) newIndex = 0;
+           this.todoList[index].status = this.statuses[newIndex];
+        }
+        //doneTodo(index) {
+        //  this.isDone = !this.isDone;
+        //}
     }
 }).mount('#app');
 
